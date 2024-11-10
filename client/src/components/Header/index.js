ee0da1b495d4509e929953ec99a8ba00cecbdfb9
Alejandro Villanueva <aavillanueva6@gmail.com>
2024-11-09 23:00:18 -0800

    add font awesome for play/pause button icon

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -12,19 +12,19 @@ const navStyle = {
 };
 const Header = () => {
   const [playingLiveStream, SetPlayingLiveStream] = useState(false);
-  const [liveStreamButtonText, SetLiveStreamButtonText] =
-    useState('Listen Live');
+  const [liveStreamButtonIcon, SetLiveStreamButtonIcon] =
+    useState('fa-solid fa-play');
 
   const handleListenLiveClick = () => {
     const audioElement = document.getElementById('liveRadioStream');
     if (!playingLiveStream) {
       audioElement.play();
       SetPlayingLiveStream(true);
-      SetLiveStreamButtonText('Pause Stream');
+      SetLiveStreamButtonIcon('fa-solid fa-pause');
     } else {
       audioElement.pause();
       SetPlayingLiveStream(false);
-      SetLiveStreamButtonText('Listen Live');
+      SetLiveStreamButtonIcon('fa-solid fa-play');
     }
   };
 
@@ -44,7 +44,7 @@ const Header = () => {
             </Navbar.Brand>
           </Link>
           <Button onClick={handleListenLiveClick} variant="outline-primary">
-            {liveStreamButtonText}
+            <i className={liveStreamButtonIcon}></i>
             <audio id="liveRadioStream" preload="none">
               <source
                 src="https://ssl-proxy.icastcenter.com/get.php?type=Icecast&server=199.180.72.2&port=9007&mount=/stream&data=mp3"
