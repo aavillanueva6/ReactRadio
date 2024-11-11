17989d53f387076de60638d59f0c81fb50d58bd2
Alejandro Villanueva <aavillanueva6@gmail.com>
2024-11-10 23:55:27 -0800

    add bootstrap customization and related scripts

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import './utils/custom.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
