38ba6aca5e1131c873a2f577393b669fe8bcc24c
Alejandro Villanueva <aavillanueva6@gmail.com>
2024-12-26 10:09:21 -0500

    add placeholder images, fix bug for referencing show hosts

--- client/src/components/DJCard/index.js
+++ client/src/components/DJCard/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const DJCard = ({ dj }) => {
+  let djImageSrc = '';
+  if (dj.image != '') {
+    djImageSrc = dj.image;
+  } else {
+    djImageSrc =
+      'https://aav-myawsbucket.s3.us-west-2.amazonaws.com/WETF-Prod/member-images/WETF_placeholder.svg';
+  }
+
   return (
     <div key={dj._id} className='mx-auto col text-center'>
       <div
         className='rounded-circle mx-auto'
         style={{
-          backgroundImage: `url('${dj.image}')`,
+          backgroundImage: `url('${djImageSrc}')`,
           backgroundPosition: 'center center',
           backgroundRepeat: 'no-repeat',
           backgroundSize: 'auto 140px',
@@ -19,7 +27,7 @@ const DJCard = ({ dj }) => {
           className='rounded-circle mx-auto'
           width='140'
           height='140'
-          src={dj.image}
+          src={djImageSrc}
           aria-label={`${dj.nickName} portrait`}
           focusable='false'
           style={{ opacity: 0.0 }}
