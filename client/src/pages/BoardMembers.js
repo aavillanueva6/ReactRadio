821d744f219f41aaf631ebb4d7aee34e4e4d9887
Alejandro Villanueva <aavillanueva6@gmail.com>
2025-01-01 23:24:12 -0800

    add Board Member page metadata

--- client/src/pages/BoardMembers.js
+++ client/src/pages/BoardMembers.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import MetaTags from 'react-meta-tags';
 import { useQuery } from '@apollo/client';
 
 import { QUERY_BOARD } from '../utils/queries';
@@ -11,12 +12,45 @@ const BoardMembers = () => {
 
   const phDJs = [0, 1, 2, 3, 4, 5, 6];
 
-  useEffect(() => {
-    document.title = 'WETF 105.7 - Board';
-  }, []);
+  const PageMetadata = {
+    meta: {
+      name: {
+        description: `Jazz Radio WETF Board Members`,
+        keywords: `WETF, Jazz`,
+        author: `Alejandro Villanueva`,
+        viewport: `width=device-width, initial-scale=1.0`,
+      },
+      property: {
+        ogTitle: `WETF 105.7 - Board`,
+        ogLocale: `en_US`,
+        ogType: `website`,
+        ogDescription: `Jazz Radio WETF Board Members`,
+      },
+    },
+  };
 
   return (
     <>
+      <MetaTags>
+        <title>WETF 105.7 - Board</title>
+        <meta name='description' content={PageMetadata.meta.name.description} />
+        <meta name='keywords' content={PageMetadata.meta.name.keywords} />
+        <meta name='author' content={PageMetadata.meta.name.author} />
+        <meta name='viewport' content={PageMetadata.meta.name.viewport} />
+        <meta
+          property='og:title'
+          content={PageMetadata.meta.property.ogTitle}
+        />
+        <meta
+          property='og:locale'
+          content={PageMetadata.meta.property.ogLocale}
+        />
+        <meta property='og:type' content={PageMetadata.meta.property.ogType} />
+        <meta
+          property='og:description'
+          content={PageMetadata.meta.property.ogDescription}
+        />
+      </MetaTags>
       <div className='container'>
         <div className='p-5 text-center'>
           <h1 className='display-4'>WETF Board Members</h1>
