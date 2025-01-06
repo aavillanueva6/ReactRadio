5bd7caf375373648343da73583356ad7a99e0254
Alejandro Villanueva <aavillanueva6@gmail.com>
2025-01-06 00:58:32 -0800

    home page prod updates

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MetaTags from 'react-meta-tags';
+import { Link } from 'react-router-dom';
 
 const homeData = require('../utils/data/homeData.json');
 
@@ -44,21 +45,77 @@ const Home = () => {
           content={PageMetadata.meta.property.ogDescription}
         />
       </MetaTags>
+      {/* banner image */}
       <img
         className={homeData.bannerImg.class}
         alt={homeData.bannerImg.alt}
         src={homeData.bannerImg.src}
         style={homeData.bannerImg.style}
       ></img>
-      <div className='container my-0'>
-        <div className='p-5 text-center bg-body-tertiary rounded-3'>
-          <h1 className='text-body-emphasis'>About WETF</h1>
-          <p className='lead'>{homeData.lead}</p>
-          {homeData.body.map((paragraph, i) => (
-            <p key={`about-paragraph-${i}`} className=''>
-              {paragraph}
-            </p>
-          ))}
+      <div className='container my-0 pt-5 px-5 bg-body-tertiary rounded-3'>
+        <h1 className={homeData.aboutText.heading.class}>
+          {homeData.aboutText.heading.text}
+        </h1>
+        {/* map section */}
+        <div className='row'>
+          <div className='col-lg-8'>
+            <img
+              className={homeData.mapSection.img.class}
+              alt={homeData.mapSection.img.alt}
+              src={homeData.mapSection.img.src}
+              style={homeData.mapSection.img.style}
+            ></img>
+          </div>
+
+          <p className={homeData.mapSection.p.class}>
+            {homeData.mapSection.p.text}
+          </p>
+        </div>
+        {/* genre section */}
+        <div className='row mt-4'>
+          <div className='col-lg-3'>
+            <img
+              className={homeData.genreSection.img.class}
+              alt={homeData.genreSection.img.alt}
+              src={homeData.genreSection.img.src}
+              style={homeData.genreSection.img.style}
+            ></img>
+          </div>
+          <div className='col-lg-9'>
+            <div className='row'>
+              <h3>{homeData.genreSection.header}</h3>
+            </div>
+            <div className='row'>
+              <ul>
+                {homeData.genreSection.ul.map((genre, i) => {
+                  return <li key={`${genre}-${i}`}>{genre}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
+          <p>
+            There is also jazz for kids, jazz history presentations and talking
+            jazz with the musicians. Check out our{' '}
+            <strong>
+              <Link to='/schedule'>Weekly Schedule</Link>
+            </strong>{' '}
+            for the times to hear your preferred music, presented by more than
+            two dozen carefully selected jazz hosts from throughout the country.
+            See the{' '}
+            <strong>
+              <Link to='/contributors'>Contributors page</Link>
+            </strong>{' '}
+            for details on our hosts.
+          </p>
+        </div>
+        {/* end section */}
+        <div className='row'>
+          <img
+            className={homeData.endSection.img.class}
+            alt={homeData.endSection.img.alt}
+            src={homeData.endSection.img.src}
+            style={homeData.endSection.img.style}
+          ></img>
         </div>
       </div>
     </>
