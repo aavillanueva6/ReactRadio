3ed490378766b27e2b9c17ad556c504a6ac49e49
Alejandro Villanueva <aavillanueva6@gmail.com>
2025-01-14 00:07:18 -0800

    update import statements

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,5 +1,4 @@
-import * as React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
