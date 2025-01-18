f9ede0d2dfca726d661f9d301b94c62d47bb6e68
Alejandro Villanueva <aavillanueva6@gmail.com>
2025-01-17 23:25:36 -0800

    add metadata, comment out bulk of page to allow for build

--- app/contributors/page.tsx
+++ app/contributors/page.tsx
@@ -1,26 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useQuery } from '@apollo/client';
+import { Metadata } from 'next';
 
-import { QUERY_DJs } from '../utils/queries';
-import DJCard from '../components/DJCard';
-import PHDJCard from '../components/PHDJCard';
+// import { QUERY_DJs } from '../utils/queries';
+// import DJCard from '../components/DJCard';
+// import PHDJCard from '../components/PHDJCard';
 
 interface PageMetadataType {
   title: string;
-  meta: {
-    name: {
-      description: string;
-      keywords: string;
-      author: string;
-      viewport: string;
-    };
-    property: {
-      ogLocale: string;
-      ogType: string;
-      ogTitle: string;
-      ogDescription: string;
-    };
-  };
+  description: string;
 }
 
 interface ShowsType {
@@ -40,33 +28,25 @@ interface DJsType {
   _id: string;
 }
 
+const pageMetaData: PageMetadataType = {
+  title: 'Contributors',
+  description: 'Jazz Radio WETF Contributors.',
+};
+
+export const metadata: Metadata = {
+  title: pageMetaData.title,
+  description: pageMetaData.description,
+  openGraph: {
+    title: pageMetaData.title,
+    description: pageMetaData.description,
+  },
+};
+
 const DJs: React.FC = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+  // const { loading, data } = useQuery(QUERY_DJs);
+  // const djs: DJsType[] = data?.djs || [];
 
-  const { loading, data } = useQuery(QUERY_DJs);
-  const djs: DJsType[] = data?.djs || [];
-
-  const phDJs = [0, 1, 2, 3, 4, 5, 6];
-
-  let PageMetadata: PageMetadataType = {
-    title: `WETF 105.7 - Contributors`,
-    meta: {
-      name: {
-        description: `Jazz Radio WETF Contributors`,
-        keywords: `WETF, Jazz`,
-        author: `Alejandro Villanueva`,
-        viewport: `width=device-width, initial-scale=1.0`,
-      },
-      property: {
-        ogLocale: `en_US`,
-        ogType: `website`,
-        ogTitle: '',
-        ogDescription: '',
-      },
-    },
-  };
+  // const phDJs = [0, 1, 2, 3, 4, 5, 6];
 
   return (
     <>
@@ -74,13 +54,13 @@ const DJs: React.FC = () => {
         <div className='p-5 text-center'>
           <h1 className='display-4'>WETF Contributors</h1>
         </div>
-        <div className='row row-cols-2 row-cols-md-3 row-cols-lg-4 justify-content-center'>
+        {/* <div className='row row-cols-2 row-cols-md-3 row-cols-lg-4 justify-content-center'>
           {loading
             ? phDJs.map((e: number) => (
                 <PHDJCard aria-hidden='true' key={`placeholder-dj-card-${e}`} />
               ))
             : djs.map((dj: DJsType) => <DJCard key={dj._id} dj={dj} />)}
-        </div>
+        </div> */}
       </div>
     </>
   );
