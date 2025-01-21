756492dbd91387b4cdeee1a546678a84645bed53
Alejandro Villanueva <aavillanueva6@gmail.com>
2025-01-21 14:05:46 -0800

    fix linting error, update rules

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,12 +1,7 @@
 import React /*, { useState }*/ from 'react';
-import { useQuery } from '@apollo/client';
 // import { QUERY_SINGLE_DAY } from '../utils/queries';
 import { Metadata } from 'next';
-import {
-  ScheduleDaysButton,
-  ScheduleShowRow,
-  PHScheduleShowRow,
-} from '@/app/components';
+import { ScheduleDaysButton } from '@/app/components';
 import { Providers } from '@/app/providers';
 
 interface PageMetadataType {
