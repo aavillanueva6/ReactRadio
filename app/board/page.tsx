c210f9ed4a0195fadec5bf278eb334d3394d176c
Alejandro Villanueva <aavillanueva6@gmail.com>
2025-01-21 23:12:00 -0800

    update for nextjs, break out client component

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
-// import { useQuery } from '@apollo/client';
 import { Metadata } from 'next';
-
-// import { QUERY_BOARD } from '../utils/queries';
-// import BMCard from '../components/BMCard';
-// import PHDJCard from '../components/PHDJCard';
+import BoardPageClient from '@/app/components/BoardPageClient';
+import { Providers } from '@/app/providers';
 
 interface PageMetadataType {
   title: string;
   description: string;
 }
-
-// interface BoardMemberType {
-//   Title: string;
-//   firstName: string;
-//   fullName: string;
-//   image: string;
-//   lastName: string;
-//   nickName: string;
-//   sqImage: string;
-//   url: string;
-//   _id: string;
-// }
 
 const pageMetaData: PageMetadataType = {
   title: 'Board',
@@ -38,26 +23,17 @@ export const metadata: Metadata = {
 };
 
 const BoardMembers: React.FC = () => {
-  // const { loading, data } = useQuery(QUERY_BOARD);
-  // let boardMembers: BoardMemberType[] = data?.boardMembers || [];
-
-  // const phDJs: number[] = [0, 1, 2, 3, 4, 5, 6];
-
   return (
     <>
       <div className='container'>
         <div className='p-5 text-center'>
           <h1 className='display-4'>WETF Board Members</h1>
         </div>
-        {/* <div className='row row-cols-2 row-cols-md-3 row-cols-lg-4 justify-content-center'>
-          {loading
-            ? phDJs.map((e: number) => (
-                <PHDJCard aria-hidden='true' key={`placeholder-dj-card-${e}`} />
-              ))
-            : boardMembers.map((member: BoardMemberType) => (
-                <BMCard key={member._id} bm={member} />
-              ))}
-        </div> */}
+        <div className='row row-cols-2 row-cols-md-3 row-cols-lg-4 justify-content-center'>
+          <Providers>
+            <BoardPageClient />
+          </Providers>
+        </div>
       </div>
     </>
   );
---
+++ app/components/BoardPageClient.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { useQuery } from '@apollo/client';
+import { QUERY_BOARD } from '@/app/lib/queries';
+import BMCard from '@/app/components/BMCard';
+import PHDJCard from '@/app/components/PHDJCard';
+
+interface BoardMemberType {
+  Title: string;
+  firstName: string;
+  fullName: string;
+  image: string;
+  lastName: string;
+  nickName: string;
+  sqImage: string;
+  url: string;
+  _id: string;
+}
+
+const BoardPageClient: React.FC = () => {
+  const { loading, data } = useQuery(QUERY_BOARD);
+  let boardMembers: BoardMemberType[] = data?.boardMembers || [];
+
+  const phDJs: number[] = [0, 1, 2, 3, 4, 5, 6];
+  return (
+    <>
+      {loading
+        ? phDJs.map((e: number) => (
+            <PHDJCard aria-hidden='true' key={`placeholder-dj-card-${e}`} />
+          ))
+        : boardMembers.map((member: BoardMemberType) => (
+            <BMCard key={member._id} bm={member} />
+          ))}
+    </>
+  );
+};
+
+export default BoardPageClient;
