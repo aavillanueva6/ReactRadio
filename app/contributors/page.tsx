9a8e35543071b047d68c92776b68602ef3293436
Alejandro Villanueva <aavillanueva6@gmail.com>
2025-01-22 21:21:44 -0800

    split to client and server components

--- app/contributors/page.tsx
+++ app/contributors/page.tsx
@@ -1,32 +1,12 @@
 import React from 'react';
-// import { useQuery } from '@apollo/client';
 import { Metadata } from 'next';
-
-// import { QUERY_DJs } from '../utils/queries';
-// import DJCard from '../components/DJCard';
-// import PHDJCard from '../components/PHDJCard';
+import { Providers } from '@/app/providers';
+import ContributorsPageClient from '@/app/components/ContributorsPageClient';
 
 interface PageMetadataType {
   title: string;
   description: string;
 }
-
-// interface ShowsType {
-//   name: string;
-//   url: string;
-// }
-
-// interface DJsType {
-//   Shows: ShowsType[];
-//   Title: string;
-//   firstName: string;
-//   image: string;
-//   lastName: string;
-//   nickName: string;
-//   sqImage: string;
-//   url: string;
-//   _id: string;
-// }
 
 const pageMetaData: PageMetadataType = {
   title: 'Contributors',
@@ -43,24 +23,15 @@ export const metadata: Metadata = {
 };
 
 const DJs: React.FC = () => {
-  // const { loading, data } = useQuery(QUERY_DJs);
-  // const djs: DJsType[] = data?.djs || [];
-
-  // const phDJs = [0, 1, 2, 3, 4, 5, 6];
-
   return (
     <>
       <div className='container'>
         <div className='p-5 text-center'>
           <h1 className='display-4'>WETF Contributors</h1>
         </div>
-        {/* <div className='row row-cols-2 row-cols-md-3 row-cols-lg-4 justify-content-center'>
-          {loading
-            ? phDJs.map((e: number) => (
-                <PHDJCard aria-hidden='true' key={`placeholder-dj-card-${e}`} />
-              ))
-            : djs.map((dj: DJsType) => <DJCard key={dj._id} dj={dj} />)}
-        </div> */}
+        <Providers>
+          <ContributorsPageClient />
+        </Providers>
       </div>
     </>
   );
